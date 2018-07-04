fa71c5f1da9568bb2e7b11c338ddf9392aab65f2
Keita Abe <keita.abe@jam3.com>
2018-07-03 23:03:56 -0400

    update

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 const threeApp = require('./lib/createThree');
 const { camera, scene, renderer, controls } = threeApp();
 
@@ -5,17 +7,17 @@ const glsl = require('glslify');
 const simpleFrag = glsl.file('./shader/shader.frag');
 const simpleVert = glsl.file('./shader/shader.vert');
 
-var geo = new THREE.BoxGeometry(1,1,1);
-var mat = new THREE.MeshBasicMaterial({ wireframe: true, color: 0xffffff })
-var box = new THREE.Mesh(geo, mat);
+let geo = new THREE.BoxGeometry(1,1,1);
+let mat = new THREE.MeshBasicMaterial({ wireframe: true, color: 0xffffff })
+let box = new THREE.Mesh(geo, mat);
 scene.add(box);
 
-var shaderMaterial =
+let shaderMaterial =
     new THREE.ShaderMaterial({
         vertexShader:   simpleVert,
         fragmentShader: simpleFrag
     });
-var box1 = new THREE.Mesh(geo, shaderMaterial)
+let box1 = new THREE.Mesh(geo, shaderMaterial)
 box1.position.x = 1.5;
 scene.add(box1);
 
