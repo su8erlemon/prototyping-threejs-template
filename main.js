2d63d60af348fe96b1c9ff1a387b01d2302b9b2b
Keita Abe <keita.abe@jam3.com>
2018-11-29 11:34:44 -0500

    reverse camera position

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
-const threeApp = require('./lib/createThree');
-const { camera, scene, renderer, controls } = threeApp();
+// Scale for retina
+const DPR = Math.min(1.5, window.devicePixelRatio);
 
-const glsl = require('glslify');
-const simpleFrag = glsl.file('./shader/shader.frag');
-const simpleVert = glsl.file('./shader/shader.vert');
+const threeApp = require("./lib/createThree");
+const { camera, scene, renderer, controls } = threeApp(DPR);
 
-let geo = new THREE.BoxGeometry(1,1,1);
-let mat = new THREE.MeshBasicMaterial({ wireframe: true, color: 0xffffff })
+const glsl = require("glslify");
+const simpleFrag = glsl.file("./shader/shader.frag");
+const simpleVert = glsl.file("./shader/shader.vert");
+
+let geo = new THREE.BoxGeometry(1, 1, 1);
+let mat = new THREE.MeshBasicMaterial({ wireframe: true, color: 0xffffff });
 let box = new THREE.Mesh(geo, mat);
 scene.add(box);
 
-let shaderMaterial =
-    new THREE.ShaderMaterial({
-        vertexShader:   simpleVert,
-        fragmentShader: simpleFrag
-    });
-let box1 = new THREE.Mesh(geo, shaderMaterial)
+let shaderMaterial = new THREE.ShaderMaterial({
+  vertexShader: simpleVert,
+  fragmentShader: simpleFrag
+});
+let box1 = new THREE.Mesh(geo, shaderMaterial);
 box1.position.x = 1.5;
 scene.add(box1);
 
 render();
 
 function render() {
-    requestAnimationFrame(render);
-    renderer.render(scene, camera)
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
 }
